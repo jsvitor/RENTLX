c1af0069f33c17f40b6b534efcc3b4ea5fb8669c
Vitor <jszvitor@gmail.com>
2021-10-01 15:12:13 -0300

    feat(main): added code for test debug configuration

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => response.json({ message: "tudo okay!" }));
+
+app.post("/create", (request, response) => {
+  const { name } = request.body;
+  return response.json({ message: "cadastrado com sucesso!" });
+});
 
 app.listen(3333, () => console.log("server is running"));
