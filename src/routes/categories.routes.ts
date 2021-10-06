96ba863a6c769636303cf9a074ac093da29c63e0
Vitor <jszvitor@gmail.com>
2021-10-06 14:55:02 -0300

    feat(categories.routes): added Category model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,27 @@
 import { Router } from "express";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = []; // um array onde dentro dele terá elementos do tipo Category
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  categories.push({ name, description });
+  // const category = new Category();
+  // category.name = name;
+  // category.description = description;
+  // poderia ser dessa forma, mas ela não é tão eficiente quanto o uso do Object.assign
+
+  const category = new Category(); // instanciando a classe para chamar o constructor
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
 
   return response.status(200).json({ categories });
 });
