d04e36813d90a8f88305ed6b9dbf5f1b8ae8dce0
Vitor <jszvitor@gmail.com>
2021-10-06 15:43:46 -0300

    feat(categories.routes): added categories repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,29 @@
+// classe responsável por entrar em contato com o banco de dados e fazer manipulações
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category(); // instanciando a classe para chamar o constructor
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,29 +1,17 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = []; // um array onde dentro dele terá elementos do tipo Category
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  // const category = new Category();
-  // category.name = name;
-  // category.description = description;
-  // poderia ser dessa forma, mas ela não é tão eficiente quanto o uso do Object.assign
 
-  const category = new Category(); // instanciando a classe para chamar o constructor
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.status(200).json({ categories });
+  return response.status(200).send();
 });
 
 export { categoriesRoutes };
