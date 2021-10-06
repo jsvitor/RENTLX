24c0be6e4d1f452e7458ee5e0cc4171a7f0fe204
Vitor <jszvitor@gmail.com>
2021-10-06 16:03:12 -0300

    feat(categories.routes): added validation to categories creation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,10 +8,17 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
   categoriesRepository.create({ name, description });
 
-  return response.status(200).send();
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  return response.status(200).json(categoriesRepository.list());
 });
 
 export { categoriesRoutes };
