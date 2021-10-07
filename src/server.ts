eb2fe2d07c2b80f5a78770d254500653f258617a
Vitor <jszvitor@gmail.com>
2021-10-07 17:11:49 -0300

    refactor(add-solid): removed old test code

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,4 @@ const app = express();
 app.use(express.json());
 app.use("/categories", categoriesRoutes);
 
-app.get("/", (request, response) => response.json({ message: "tudo okay!" }));
-
-app.post("/create", (request, response) => {
-  const { name } = request.body;
-  return response.json({ message: "cadastrado com sucesso!" });
-});
-
 app.listen(3333, () => console.log("server is running"));
