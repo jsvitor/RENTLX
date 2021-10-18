883fb490d5f68d3f50fb377f2b6755ed05890383
Vitor <jszvitor@gmail.com>
2021-10-18 11:17:49 -0300

    feat(main): added controllers to routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories/index";
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
