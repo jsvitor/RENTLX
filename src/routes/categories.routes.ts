e3d0668f656d8aee561743b5d02b6f08d36bf0ff
Vitor <jszvitor@gmail.com>
2021-10-22 14:20:44 -0300

    feat(multer): added multer uploads structure

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories/index";
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +15,15 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  "/imports",
+  upload.single("file"),
+  (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
+  }
+);
 
 export { categoriesRoutes };
