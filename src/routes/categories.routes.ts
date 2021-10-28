4c528f123d799e16e9c17a27e62a5281c926eee0
Vitor <jszvitor@gmail.com>
2021-10-28 18:58:59 -0300

    feat(main): created importCategories use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { importCategoriesController } from "../modules/cars/useCases/importCategories";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories/index";
 
 const categoriesRoutes = Router();
@@ -20,9 +21,7 @@ categoriesRoutes.post(
   "/imports",
   upload.single("file"),
   (request, response) => {
-    const { file } = request;
-    console.log(file);
-    return response.send();
+    return importCategoriesController.handle(request, response);
   }
 );
 
